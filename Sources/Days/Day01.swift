f8ad6bd9ca61f8df1e33e952506488ed6d899740
Dustyn August <dustyn.august@glooko.com>
2023-12-03 10:34:55 -0800

    Day 1 part 2 solution

--- Sources/Days/Day01.swift
+++ Sources/Days/Day01.swift
@@ -5,21 +5,118 @@ struct Day01: AdventDay {
     var data: String
     
     // Splits input data into its component parts and convert from string.
-    var entities: [[Int]] {
-        data.split(separator: "\n\n").map {
-            $0.split(separator: "\n").compactMap { Int($0) }
-        }
-    }
+    var entities: [String] { data.split(separator: "\n").compactMap(String.init) }
+    
+    private let numbersMap = [
+        "one": 1,
+        "1": 1,
+        "two": 2,
+        "2": 2,
+        "three": 3,
+        "3": 3,
+        "four": 4,
+        "4": 4,
+        "five": 5,
+        "5": 5,
+        "six": 6,
+        "6": 6,
+        "seven": 7,
+        "7": 7,
+        "eight": 8,
+        "8": 8,
+        "nine": 9,
+        "9": 9,
+    ]
     
     // Replace this with your solution for the first part of the day's challenge.
     func part1() -> Any {
-        // Calculate the sum of the first set of input data
-        "TODO"
+        // On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number. 
+        // Digits include: "one", "1", "two", "2", "three", "3" ,..., "nine", "9"
+        // Find the sum of each single two-digit number.
+        var sum = 0
+        for entity in entities {
+            let ints = entity.filter("0123456789.".contains)
+                
+            // Get all the Int Strings
+            guard let firstDigit = ints.first,
+                  let lastDigit = ints.last,
+                  let number = Int("\(firstDigit)\(lastDigit)")
+            else {
+                continue
+            }
+            
+            sum = sum + number
+        }
+        
+        return sum
     }
     
     // Replace this with your solution for the second part of the day's challenge.
     func part2() -> Any {
-        // Sum the maximum entries in each set of data
-        "TODO"
+        // On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
+        // Digits are "0", 1", "2", ... "9"
+        // Find the sum of each single two-digit number.
+        var sum = 0
+        for entity in entities {
+            var firstDigitString: String? = nil
+            var secondDigitString: String? = nil
+            
+            var left = 0
+            var right = 0
+            let chars = Array<Character>(entity)
+            let boundary = entity.count - 1
+            
+            while right <= boundary {
+                while right <= boundary,
+                      let substring = String(chars[right...right]) as String?,
+                      !potentialMatch(at: substring) {
+                    right = right + 1
+                }
+                
+                left = right
+                
+                while right <= boundary,
+                      let substring = String(chars[left...right]) as String?,
+                      potentialMatch(at: substring) {
+                    
+                    if numbersMap.keys.contains(substring) {
+                        if firstDigitString == nil { firstDigitString = substring }
+                        secondDigitString = substring
+                        // If substring is a number advance ahead by one position.
+                        if "123456789".contains(substring) { right = right + 1 }
+                        left = right
+                        break
+                        
+                    } else {
+                        right = right + 1
+                    }
+                }
+                
+                // No potential match found but need to keep right within one position of left
+                if right - left > 1 {
+                    right = right - 1
+                }
+            }
+            
+            guard
+                let firstDigit = numbersMap[firstDigitString ?? ""],
+                let secondDigit = numbersMap[secondDigitString ?? ""],
+                let number = Int("\(firstDigit)\(secondDigit)") 
+            else {
+                firstDigitString = nil
+                secondDigitString = nil
+                continue
+            }
+            
+            sum = sum + number
+            firstDigitString = nil
+            secondDigitString = nil
+        }
+        
+        return sum
+    }
+    
+    private func potentialMatch(at string: String) -> Bool {
+        numbersMap.keys.contains(where: { $0.starts(with: string) })
     }
 }
--- Tests/Days/Day01Tests.swift
+++ Tests/Days/Day01Tests.swift
@@ -4,25 +4,125 @@ import XCTest
 
 // Make a copy of this file for every day to ensure the provided smoke tests
 // pass.
-final class Day01Tests: XCTestCase {
+final class Day01Tests: XCTestCase { }
+
+// MARK: - Part 1 Test(s)
+extension Day01Tests {
     func testPart1() throws {
-        let challenge = Day01(data: testData)
+        let challenge = Day01(data: part1_TestData)
         XCTAssertEqual(String(describing: challenge.part1()), "142")
     }
     
-    func testPart2() throws {
-        XCTFail("TODO")
+    func test_part1_TestData_Line1() throws {
+        let challenge = Day01(data: part1_TestData_Line1)
+        XCTAssertEqual(String(describing: challenge.part1()), "12")
     }
+    
+    func test_part1_TestData_Line2() throws {
+        let challenge = Day01(data: part1_TestData_Line2)
+        XCTAssertEqual(String(describing: challenge.part1()), "38")
+    }
+    
+    func test_part1_TestData_Line3() throws {
+        let challenge = Day01(data: part1_TestData_Line3)
+        XCTAssertEqual(String(describing: challenge.part1()), "15")
+    }
+    
+    func test_part1_TestData_Line4() throws {
+        let challenge = Day01(data: part1_TestData_Line4)
+        XCTAssertEqual(String(describing: challenge.part1()), "77")
+    }
+    
+    // MARK: Part 1 Test Data
+    private var part1_TestData: String {
+        return """
+        \(part1_TestData_Line1)
+        \(part1_TestData_Line2)
+        \(part1_TestData_Line3)
+        \(part1_TestData_Line4)
+        """
+    }
+    
+    private var part1_TestData_Line1: String { "1abc2" }
+    private var part1_TestData_Line2: String { "pqr3stu8vwx" }
+    private var part1_TestData_Line3: String { "a1b2c3d4e5f" }
+    private var part1_TestData_Line4: String { "treb7uchet" }
 }
 
-private extension Day01Tests {
-    // Smoke test data provided in the challenge question
-    private var testData: String {
-        return """
-      1abc2
-      pqr3stu8vwx
-      a1b2c3d4e5f
-      treb7uchet
-      """
+// MARK: - Part 2 Test(s)
+extension Day01Tests {
+    func test_part2() throws {
+        let challenge = Day01(data: part2_TestData)
+        XCTAssertEqual(String(describing: challenge.part2()), "281")
     }
+    
+    func test_part2_TestData_Line1() throws {
+        let challenge = Day01(data: part2_TestData_Line1)
+        XCTAssertEqual(String(describing: challenge.part2()), "29")
+    }
+    
+    func test_part2_TestData_Line2() throws {
+        let challenge = Day01(data: part2_TestData_Line2)
+        XCTAssertEqual(String(describing: challenge.part2()), "83")
+    }
+    
+    func test_part2_TestData_Line3() throws {
+        let challenge = Day01(data: part2_TestData_Line3)
+        XCTAssertEqual(String(describing: challenge.part2()), "13")
+    }
+    
+    func test_part2_TestData_Line4() throws {
+        let challenge = Day01(data: part2_TestData_Line4)
+        XCTAssertEqual(String(describing: challenge.part2()), "24")
+    }
+    
+    func test_part2_TestData_Line5() throws {
+        let challenge = Day01(data: part2_TestData_Line5)
+        XCTAssertEqual(String(describing: challenge.part2()), "42")
+    }
+    
+    func test_part2_TestData_Line6() throws {
+        let challenge = Day01(data: part2_TestData_Line6)
+        XCTAssertEqual(String(describing: challenge.part2()), "14")
+    }
+    
+    func test_part2_TestData_Line7() throws {
+        let challenge = Day01(data: part2_TestData_Line7)
+        XCTAssertEqual(String(describing: challenge.part2()), "76")
+    }
+    
+    
+    func test_part2_TestData_OverlappingSpelledOutDigit() throws {
+        let challenge = Day01(data: "twone")
+        XCTAssertEqual(String(describing: challenge.part2()), "21")
+    }
+    
+    
+    func test_part2_TestData_AdHoc() throws {
+        let challenge = Day01(data: "twonine")
+        XCTAssertEqual(String(describing: challenge.part2()), "29")
+    }
+    
+    
+    // MARK: Part 2 Test Data
+    
+    private var part2_TestData: String {
+        return """
+        \(part2_TestData_Line1)
+        \(part2_TestData_Line2)
+        \(part2_TestData_Line3)
+        \(part2_TestData_Line4)
+        \(part2_TestData_Line5)
+        \(part2_TestData_Line6)
+        \(part2_TestData_Line7)
+        """
+    }
+    
+    private var part2_TestData_Line1: String { "two1nine" }
+    private var part2_TestData_Line2: String { "eightwothree" }
+    private var part2_TestData_Line3: String { "abcone2threexyz" }
+    private var part2_TestData_Line4: String { "xtwone3four" }
+    private var part2_TestData_Line5: String { "4nineeightseven2" }
+    private var part2_TestData_Line6: String { "zoneight234" }
+    private var part2_TestData_Line7: String { "7pqrstsixteen" }
 }
