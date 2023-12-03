fb428c63791506a72d8117c4f101aeb892f5dfd4
Dustyn August <dustyn.august@glooko.com>
2023-12-03 10:47:09 -0800

    Day02 setup.

---
+++ Tests/Days/TemplateTests.swift
@@ -0,0 +1,34 @@
+import XCTest
+
+@testable import AdventOfCode
+
+// Make a copy of this file for every day to ensure the provided smoke tests
+// pass.
+final class TemplateTests: XCTestCase { }
+
+// MARK: - Part 1 Test(s)
+extension TemplateTests {
+    func test_part1() throws {
+        // TODO: Add Test for Part 1
+    }
+    
+    // MARK: Part 1 Test Data
+    private var part1_TestData: String {
+        return """
+        """
+    }
+}
+
+// MARK: - Part 2 Test(s)
+extension TemplateTests {
+    func test_part2() throws {
+        // TODO: Add Test for Part 2
+    }
+    
+    // MARK: Part 2 Test Data
+    
+    private var part2_TestData: String {
+        return """
+        """
+    }
+}
