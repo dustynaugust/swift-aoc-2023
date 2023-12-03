c552196fb54752f8df28def7b8114288aaa1c60f
Dustyn August <dustyn.august@glooko.com>
2023-12-02 17:29:53 -0800

    Setup for Day01 part 1.

---
+++ Tests/Days/Day01Tests.swift
@@ -0,0 +1,28 @@
+import XCTest
+
+@testable import AdventOfCode
+
+// Make a copy of this file for every day to ensure the provided smoke tests
+// pass.
+final class Day01Tests: XCTestCase {
+  func testPart1() throws {
+    let challenge = Day01(data: testData)
+    XCTAssertEqual(String(describing: challenge.part1()), "142")
+  }
+
+  func testPart2() throws {
+    XCTFail("TODO")
+  }
+}
+
+private extension Day01Tests {
+    // Smoke test data provided in the challenge question
+    private var testData: String {
+        return """
+      1abc2
+      pqr3stu8vwx
+      a1b2c3d4e5f
+      treb7uchet
+      """
+    }
+}
